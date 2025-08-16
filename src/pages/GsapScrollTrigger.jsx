15814be6d4ab8c0cf1bbdcf3a19a4927e64b2d4e
Panvish Dowripilli <panvishd@gmail.com>
2025-08-17 01:57:27 +0530

    GsapScrollTrigger done

--- src/pages/GsapScrollTrigger.jsx
+++ src/pages/GsapScrollTrigger.jsx
@@ -1,5 +1,41 @@
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
+import { ScrollTrigger } from "gsap/all";
+import { useRef } from "react";
+
+gsap.registerPlugin(ScrollTrigger);
+
 const GsapScrollTrigger = () => {
-  // TODO: Implement the gsap scroll trigger
+
+  // gsap scroll trigger is implemented here (Try modifying it)
+  // ScrollTrigger is a plugin so we need to import it and register
+  // Trigger the animation in scroll
+  const scrollRef = useRef();
+  // scrolleRef is used in the boxes div
+
+  useGSAP(() => {
+    const boxes = gsap.utils.toArray(scrollRef.current.children);
+
+    boxes.forEach((box) => {
+      gsap.to(box, {
+        x: 150 * (boxes.indexOf(box) + 5),
+        rotation: 360,
+        borderRadius: '100%',
+        scale: 1.5,
+        scrollTrigger: {
+          trigger: box,
+          start: 'bottom bottom',
+          end: 'top 10%',
+          // scrub is used for smooth animations
+          scrub: true,
+        },
+        ease: 'power1.inOut'
+      })
+    })
+    // We have passed scrollRef as dependency to identify the reference to the element
+  }, {scope: scrollRef})
+
+ 
 
   return (
     <main>
@@ -51,7 +87,7 @@ const GsapScrollTrigger = () => {
         </svg>
       </div>
 
-      <div className="mt-20 w-full h-screen">
+      <div className="mt-20 w-full h-screen" ref={scrollRef}>
         <div
           id="scroll-pink"
           className="scroll-box w-20 h-20 rounded-lg bg-pink-500"
